9d7b9335a587d4af74b032c50e2bbc9232175766
SophieZheng <bmzheng.sophie@gmail.com>
2013-09-05 15:04:45 +0800

    initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rorsa::Application.config.secret_key_base = 'a5dba5602a240abac842f005f20a6d52395989e9b97c2408d55deb871a6ee4ace3676b9f0041be97aa51f1e4527b855894b1ecee8b583b478d7aa9d1df422f7d'
